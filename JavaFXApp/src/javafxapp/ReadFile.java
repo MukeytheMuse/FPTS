9bc1ce9c50e56dfcd4fb0f5d1e1f2408d8b1f34d
ericethegreat <eric.samuel.epstein@gmail.com>
2016-02-18 22:32:34 -0500

    Added JavaFX functionalities

---
+++ JavaFXApp/src/javafxapp/ReadFile.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafxapp;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Created by kimberlysookoo on 2/17/16.
+ */
+public class ReadFile {
+    private ArrayList<String[]> splitFile;
+
+    public ReadFile() {
+        this.splitFile = new ArrayList<String[]>();
+    }
+
+    //Reads in CSV file
+    public void readFile() {
+        //String filePath = "/Users/kimberlysookoo/Desktop/rit-challenge/transactions-person-A.csv";
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter file name: ");
+        String csv = userInput.nextLine();
+        BufferedReader reader = null;
+        String line = "";
+
+        try {
+            reader = new BufferedReader(new FileReader(csv));
+            while ((line = reader.readLine()) != null) {
+
+                String[] split = line.split(",");
+                splitFile.add(split);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("That file does not exist! Please try again.");
+            readFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
