3b39796113a80fb3ea02df1fe8eddecae59a6746
MukeytheMuse <kimsookoo121@gmail.com>
2016-02-26 19:29:11 -0500

    Main file that was there before has been put back.

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by kimberlysookoo on 2/26/16.
+ */
+public class Main {
+
+    public static void main(String [ ] args) {
+        ReadFile readFile = new ReadFile();
+        readFile.readFile();
+    }
+}
